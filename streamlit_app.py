d812e47b6ae8b4aa2e91a6fd89b995cae1ffeecb
rjone199 <163476005+rjone199@users.noreply.github.com>
2024-12-09 15:21:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def home_page():
                     date = event.get("dates", {}).get("start", {}).get("localDate", "N/A")
                     venue = event.get("_embedded", {}).get("venues", [{}])[0].get("name", "N/A")
 
-            if st.button(f"Select: {name} at {venue} on {date}"):
+                if st.button(f"Select: {name} at {venue} on {date}"):
                         st.write("name: ", name, "date:",   date ,"venue:", venue)
                         choiceName = name
                         choiceDate = date
